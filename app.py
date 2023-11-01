f626a9c9d912349ec9a80b51fa45b9582622d7de
Nikhil Tamvada <117595173+Nikhilodeon1@users.noreply.github.com>
2023-10-31 22:43:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response, session, abort
 import json
+import requests
 import os.path
+from roboflow import Roboflow
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 #app.run(debug=True)
@@ -11,83 +13,59 @@ app.secret_key = "|\|||<|-|||_"
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if session:
-    return render_template('main2.html')
+    return redirect(url_for('destination'))
   else:
-    return render_template('main.html')
+    return render_template('home.html')
 
-@app.route('/progress', methods=['GET', 'POST'])
-def progress():
+@app.route('/p', methods=['POST'])
+def p():
+  long = request.form['long']
+  lat = request.form['lat']
+  # Define the zoom level and image size
+  zoom = 20 # 1-20
+  size = 640 # 1-640
+
+  # Define the base URL for Bing Maps API
+  base_url = "https://dev.virtualearth.net/REST/V1/Imagery/Map/Aerial"
+
+  # Construct the full URL with parameters
+  full_url = f"{base_url}/{lat},{long}/{zoom}?mapSize={size},{size}&key=AldazXMMRfAD8Jg5w-XqpXM7wFjNP1poTOP52Q8LViFaRiqPofN2e8wWgonN0SHu"
+
+  # Get the image response from the API
+  response = requests.get(full_url)
+
+  # Check if the response is successful
+  if response.status_code == 200:
+      # Save the image to a file
+      with open("satellite_image.jpg", "wb") as f:
+          f.write(response.content)
+
+  rf = Roboflow(api_key="4RuIXtKeuz8R4qIyL19Y")
+  project = rf.workspace().project("pl-psq41")
+  model = project.version(1).model
+
+  # infer on a local image
+  result = model.predict("satellite_image.jpg", confidence=0, overlap=30).json()
+  print(result)
+  print(len(result['predictions']))
+  print("DOOOOOOOONEEEEEEEEEE")
+  if len(result['predictions']) > 0:
+    session['lat']=lat
+    session['long']=long
+    return redirect(url_for('destination'))
+  else:
+    return redirect(url_for('home'))
+
+@app.route('/destination', methods=['GET', 'POST'])
+def destination():
   if session:
-    return render_template('progress.html')
+    return render_template('destination.html', lat=session['lat'], long=session['long'])
   else:
-    return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
-@app.route('/aibot', methods=['GET', 'POST'])
-def aibot():
-  if session:
-    return render_template('aibot.html')
-  else:
-    return redirect(url_for('login'))
-    
-@app.route('/music', methods=['GET', 'POST'])
-def music():
-  if session:
-    return render_template('music.html')
-  else:
-    return redirect(url_for('login'))
-    
-@app.route('/you', methods=['GET', 'POST'])
-def you():
-  if session:
-    return render_template('you.html')
-  else:
-    return redirect(url_for('login'))
-  
-@app.route('/logbook', methods=['GET', 'POST'])
-def logbook():
-  if session:
-    return render_template('logbook.html')
-  else:
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  return render_template('login.html')
-
-@app.route('/newacc', methods=['GET', 'POST'])
-def newacc():
-  return render_template('newacc.html')
-
-@app.route('/setsession', methods=['GET', 'POST'])
-def setsession():
-  uname = request.form['uname']
-  psw = request.form['psw']
-  if request.form['from'] == "login":
-    if os.path.isfile('users/'+uname+'.json'):
-      f = open('users/'+uname+'.json')
-      data = json.load(f)
-      f.close()
-      if data['psw'] == psw:
-        session['uname'] = uname
-        session['psw'] = psw
-        return redirect(url_for('home'))
-
-      else:
-        flash('Wrong password')
-        return redirect(url_for('login'))
-    else:
-      flash('User not found')
-      return redirect(url_for('login'))
-  else:
-    with open("users/" + uname + ".json", "w") as file:
-      dicta= {'psw': psw}
-      json.dump(dicta, file)
-      session['uname'] = uname
-      session['psw'] = psw
-      return redirect(url_for('home'))
-
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
+@app.route('/delsession', methods=['GET', 'POST'])
+def delsession():
   session.clear()
   return redirect(url_for('home'))
+
 app.run(host='0.0.0.0', port=81)
